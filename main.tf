3bbdeeffbae55f75081678299c5e84f649717efc
Priyanka <priyankaponnam123@gmail.com>
2024-08-01 16:28:48 -0400

    main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project = "polar-office-422517-t9"
+  region  = "us-central1"  # Specify the region if needed for other resources
+}
+
+resource "google_storage_bucket" "my_bucket" {
+  name     = "bucket251194"  # Must be globally unique
+  location = "US"                      # Location of the bucket
+
+  lifecycle {
+    prevent_destroy = true             # Optional: Prevent the bucket from being accidentally destroyed
+  }
+}
